b28176a5a2bd41a92c7807074c023b32b34e697c
Bezaleel <bezaleel5work@gmail.com>
2026-02-20 23:55:55 +0530

    assign elastic ip to frontend

--- frontend/src/utils/Instance.js
+++ frontend/src/utils/Instance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://13.61.81.114",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
