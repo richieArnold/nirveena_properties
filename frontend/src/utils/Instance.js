a11d19a21232092fe61e1d7e406757246d4931a6
Bezaleel <bezaleel5work@gmail.com>
2026-02-28 23:59:16 +0530

    proxy update

--- frontend/src/utils/Instance.js
+++ frontend/src/utils/Instance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://13.61.81.114:5000",
+  baseURL: "https://d1guf7qylta6z5.cloudfront.net/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
