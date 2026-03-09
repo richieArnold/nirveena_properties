b9dcd2efc2d4f2b2bc53fd02ba79488e7a4478da
Bezaleel <bezaleel5work@gmail.com>
2026-03-10 01:19:03 +0530

    Seo-01

--- backend/routes/sitemap.js
+++ backend/routes/sitemap.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const pool = require('../rds_setup/db/index');
+const pool = require("../db");
 
 router.get("/sitemap.xml", async (req, res) => {
   try {
 
-    const result = await pool.query(
-      "SELECT slug FROM projects"
-    );
+    const result = await pool.query("SELECT slug FROM projects");
 
     const baseUrl = "https://www.nirveena.com";
 
-    let urls = `
-      <url>
-        <loc>${baseUrl}</loc>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/property</loc>
-        <priority>0.9</priority>
-      </url>
-    `;
+    let urls = "";
 
     result.rows.forEach(property => {
       urls += `
-        <url>
-          <loc>${baseUrl}/property/${property.slug}</loc>
-          <priority>0.8</priority>
-        </url>
-      `;
+<url>
+  <loc>${baseUrl}/property/${property.slug}</loc>
+  <priority>0.8</priority>
+</url>`;
     });
 
-    const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls}
-      </urlset>
-    `;
+    const sitemap =
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+  <loc>${baseUrl}</loc>
+  <priority>1.0</priority>
+</url>
+<url>
+  <loc>${baseUrl}/property</loc>
+  <priority>0.9</priority>
+</url>
+${urls}
+</urlset>`;
 
     res.header("Content-Type", "application/xml");
     res.send(sitemap);
