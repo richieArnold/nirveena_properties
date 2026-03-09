c0d5c318bccd666d2e5fb3e0fd1b818748d125ed
Bezaleel <bezaleel5work@gmail.com>
2026-03-10 00:52:14 +0530

    SEO 01

---
+++ backend/routes/sitemap.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const pool = require('../rds_setup/db/index');
+
+router.get("/sitemap.xml", async (req, res) => {
+  try {
+
+    const result = await pool.query(
+      "SELECT slug FROM properties WHERE published = true"
+    );
+
+    const baseUrl = "https://www.nirveena.com";
+
+    let urls = `
+      <url>
+        <loc>${baseUrl}</loc>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/property</loc>
+        <priority>0.9</priority>
+      </url>
+    `;
+
+    result.rows.forEach(property => {
+      urls += `
+        <url>
+          <loc>${baseUrl}/property/${property.slug}</loc>
+          <priority>0.8</priority>
+        </url>
+      `;
+    });
+
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+      </urlset>
+    `;
+
+    res.header("Content-Type", "application/xml");
+    res.send(sitemap);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error generating sitemap");
+  }
+});
+
+module.exports = router;
