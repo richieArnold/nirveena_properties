11d58f06d17bb4b4d57f7f9fc82ce48ec6d1f95a
Bezaleel <bezaleel5work@gmail.com>
2026-03-16 23:14:11 +0530

    Final 01

--- frontend/src/utils/Instance.js
+++ frontend/src/utils/Instance.js
@@ -2,14 +2,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://api.nirveena.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://api.nirveena.com/",
+  // baseURL: "http://localhost:5000/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
+5
 
 // Add token to every request automatically
 axiosInstance.interceptors.request.use((config) => {
